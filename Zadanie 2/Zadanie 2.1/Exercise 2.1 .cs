efd89390b62f657531b2f128593c1aa7d7baf79a
DanilaSiniak <sinyak2002@gmail.com>
2021-02-28 15:56:09 +0300

    add zadanie 2

---
+++ Zadanie 2/Zadanie 2.1/Exercise 2.1 .cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Zad2
+{
+    class Program
+    {
+       	static void Main(string[] args)
+	        {
+	            Console.Write("Введите десятичное число: ");
+	            int i = Convert.ToInt32(Console.ReadLine());
+                string s = Convert.ToString(i, 2);
+	            Console.WriteLine("В двоичной системе счисления это: "+s);
+	            Console.ReadLine();
+
+	        }
+
+    }
+}
+
+        
+
+
