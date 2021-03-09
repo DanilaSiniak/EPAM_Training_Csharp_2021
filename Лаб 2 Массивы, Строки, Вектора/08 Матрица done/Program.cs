649626ea3e4051d35c29f5844550d5c0b31f115e
DanilaSiniak <sinyak2002@gmail.com>
2021-03-10 00:40:18 +0300

    add LabMatrica

---
+++ Лаб 2 Массивы, Строки, Вектора/08 Матрица done/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Program
+
+
+{
+    static void Main(string[] args)
+    {
+
+        StreamReader inputFile = new StreamReader("Input.txt");
+
+        string[] s = File.ReadAllLines("Input.txt");
+        int[][] array = (from i in s
+                         select i.Trim().Split(' ').
+                         Select(x => int.Parse(x.Trim())).ToArray()).ToArray();
+        for (int i = 0; i < 7; i++)
+        {
+            for (int j = 0; j < 6; j++)
+            {
+                Console.Write(array[i][j]);
+            }
+            Console.WriteLine();
+        }
+
+
+
+        // получаем кол-во строк 
+        int length1 = 7;
+        // получаем кол-во столбцов 
+        int length2 = 6;
+        //объявляем коллекции для хранения нулевых строк и столбцов
+        List<int> ls1 = new List<int>();
+        List<int> ls2 = new List<int>();
+
+        Console.WriteLine("======Исходная матрица=========");
+        // выводим матрицу на консоль и находим нулевые строки
+        for (int i = 0; i < length1; ++i)
+        {
+            bool b = false;
+            for (int j = 0; j < length2; ++j)
+            {
+                if (array[i][j] >= 0)
+                    Console.Write("  " + array[i][j]);
+                else Console.Write(" " + array[i][j]);
+                if (array[i][j] != 0) b = true;
+            }
+            if (!b) ls1.Add(i);
+            Console.WriteLine();
+        }
+        //  находим нулевые столбцы
+        for (int i = 0; i < length1; ++i)
+        {
+            bool b = false;
+            for (int j = 0; j < length2; ++j)
+            {
+
+                if (array[i][j] != 0) b = true;
+            }
+            if (!b) ls2.Add(i);
+
+        }
+        //  Удаляем нулевые строки и столбцы и находим номер первой из строк (начиная с нуля),
+        // содержащих хотя бы один положительный элемент
+        bool B = false; int? Istr = null;
+        Console.WriteLine("======Удаляем нулевые строки и столбцы=========");
+        for (int i = 0; i < length1; ++i)
+        {
+            if (!ls1.Contains(i))
+            {
+                for (int j = 0; j < length2; ++j)
+                {
+                    if (!ls2.Contains(j))
+                    {
+                        if (array[i][j] >= 0)
+                        {
+                            if (!B)
+                            {
+                                Istr = i;
+                                B = true;
+                            }
+                            Console.Write("  " + array[i][j]);
+                        }
+                        else Console.Write(" " + array[i][j]);
+                    }
+
+                }
+
+            }
+            if (!ls1.Contains(i))
+                Console.WriteLine();
+        }
+            for (var g = 0; g < 7; g++)
+            {
+                for (var j = 0; j < 6; j++)
+                {
+
+                    Console.WriteLine(array[g][j]);
+                }
+            }
+        Console.Read();
+
+        s = File.ReadAllLines("Input.txt");
+        array = (from i in s
+                         select i.Trim().Split(' ').
+                         Select(x => int.Parse(x.Trim())).ToArray()).ToArray();
+        using (StreamWriter sw = new StreamWriter("Output.txt",false,System.Text.Encoding.Default))
+        {
+            string same = "";
+            for (int i = 0; i < 7; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    same += array[i][j] + " ";
+                }
+                sw.WriteLine(same);
+                same = "";
+            }
+        }
+
+    }
+}
