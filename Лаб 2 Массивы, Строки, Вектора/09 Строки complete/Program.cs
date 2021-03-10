e25558280d00196fb80bba5acf6c26468eb0fc78
DanilaSiniak <sinyak2002@gmail.com>
2021-03-10 18:54:44 +0300

    some changes

---
+++ Лаб 2 Массивы, Строки, Вектора/09 Строки complete/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace laba2string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var counter = 0;
+            using (var sr = new StreamReader("Input.txt"))
+            {
+                var textString = sr.ReadToEnd();
+                var text = textString.Split(' ');
+                string symbol = textString.Substring(textString.Length - 1);
+                for (int i = 0; i < text.Length; i++)
+                {
+                    if (text[i].EndsWith(symbol))
+                        counter++;
+                }
+            }
+            using (var sw = new StreamWriter("Output.txt"))
+            {
+                if (counter == 1)
+                    sw.WriteLine(-1);
+                else
+                    sw.WriteLine(counter - 1);
+            }
+        }
+
+    }
+}
