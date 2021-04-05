af89629188897bc76c775e27ad14b0b5b67ee973
DanilaSiniak <sinyak2002@gmail.com>
2021-04-05 11:26:38 +0300

    add delegate zadanie 3

---
+++ Лаб 7 Делегаты/Задание 3/Program.cs
@@ -0,0 +1,53 @@
+﻿using DelegateZadanie3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace DelegateZadanie3
+{
+    class Program : EventGenerator
+    {
+        const int EVENTCOUNTER = 10;
+        static void Main(string[] args)
+        {
+            EventGenerator generator = new EventGenerator();
+            generator.GeneratorEvent += new EventHandler(generator_GeneratorEvent);
+            generator.StartGenerate();
+            Console.ReadLine();
+        }
+
+        static void generator_GeneratorEvent(object sender, EventArgs e)
+        {
+            EventGenerator g = sender as EventGenerator;
+            if (g != null & g.GenerationCount == 0)
+            {
+                g.GenerationCount = EVENTCOUNTER;
+            }
+            Console.WriteLine("Got Event!");
+        }
+    }
+    
+
+}
+public class EventGenerator
+{
+    public event EventHandler GeneratorEvent = delegate { };
+    public int GenerationCount { get; set; }
+    public static TwoParametrEventArgs pTP = new TwoParametrEventArgs();
+    public void StartGenerate()
+    {
+        do
+        {
+            GeneratorEvent(this, pTP);
+            if (GenerationCount > 0)
+            {
+                GenerationCount -= 1;
+            }
+            Thread.Sleep(1000);
+        }
+        while (GenerationCount != 0);
+
+    }
+}
